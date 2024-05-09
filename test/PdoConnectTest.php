8c33b7c083b65072fd6cc38488a25f39b8eee8a4
a27lis <anastasiya.liseykina@gmail.com>
2024-05-09 12:48:53 +0400

    test

--- test/PdoConnectTest.php
+++ test/PdoConnectTest.php
@@ -3,11 +3,10 @@ use PHPUnit\Framework\TestCase;
 
 class PdoConnectTest extends TestCase {
 
-    public function testPdoConnect {
-        spl_autoload_register(function ($class) {
-            include 'classes/' . $class . '.php';
-        });
-        
-        $PDO = PdoConnect::getInstance();
+    public function testGetInstance()
+    {
+        $pdoConnect = PdoConnect::getInstance();
+        $this->assertInstanceOf(PdoConnect::class, $pdoConnect);
     }
+    
 }
