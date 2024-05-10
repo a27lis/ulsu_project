0f13595ac42847bbfd18a855adecc4db7d646641
a27lis <anastasiya.liseykina@gmail.com>
2024-05-10 11:51:49 +0400

    upd cicd with test

--- test/PdoConnectTest.php
+++ test/PdoConnectTest.php
@@ -5,6 +5,9 @@ class PdoConnectTest extends TestCase {
 
     public function testGetInstance()
     {
+        spl_autoload_register(function ($class) {
+            include 'classes/' . $class . '.php';
+        });
         $pdoConnect = PdoConnect::getInstance();
         $this->assertInstanceOf(PdoConnect::class, $pdoConnect);
     }
