b151c62230a8ef6c26a6d1aab5869cff546b3944
a27lis <anastasiya.liseykina@gmail.com>
2024-05-10 12:32:19 +0400

    upd test

---
+++ test/GalleryTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class GalleryTest extends TestCase
+{
+    public function testAddition()
+    {
+        $result = 5+3;
+        $this->assertEquals(8, $result);
+    }
+}
